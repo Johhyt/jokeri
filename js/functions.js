4e6806a709dc38ccbe1cb63322dc631fc1af77bc
johhyt <t2yrjo00@students.oamk.fi>
2024-02-13 15:23:04 +0200

    wuuui

---
+++ js/functions.js
@@ -0,0 +1,19 @@
+const button = document.querySelector('button')
+const table = document.querySelector('table')
+const p = document.querySelector('p')
+
+let rivit = 0
+
+const getRandomIntNumberInRange = (min, max) => {
+    return Math.floor(Math.random() * max) + min;
+}
+
+document.querySelector('button').addEventListener('click',() => {
+    const row = table.insertRow()
+    for (let i = 0;i < 7;i++) {
+        const col = row.insertCell()
+        col.innerHTML = getRandomIntNumberInRange(0, 9)
+    }
+    rivit++
+    p.innerHTML = 'Valmiita rivejä ' + rivit
+})
